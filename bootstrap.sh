017474f36add6b4c199be71ff9b70a5ee65b5090
Matt Tarbit <mtarbit@gmail.com>
2012-10-27 12:56:12 +0100

    Add --dry-run option and usage instructions.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-source_dir="$(dirname ${BASH_SOURCE[0]})/source"
-target_dir=$HOME
-manifest=Manifest
+script_dir="$(dirname "${BASH_SOURCE[0]}")"
+script="$(basename "${BASH_SOURCE[0]}")"
 
-grn=1
-red=2
+source_dir="$script_dir/source"
+target_dir=$HOME
+manifest='Manifest'
+
+red=1
+grn=2
 ylw=3
 
 function color {
@@ -22,15 +25,19 @@ function create_symlink {
     echo -e $(color $ylw "Existing symlink: $target")
   elif [ ! -e $source ]; then
     echo -e $(color $red "Error - Source does not exist: $source")
-    echo -e $(color $red "Correct path or remove from $manifest to continue")
+    echo -e $(color $red "Correct path or remove from $manifest to continue.")
     exit
   elif [ -e $target ]; then
     echo -e $(color $red "Error - Regular file at target location: $target")
-    echo -e $(color $red "Remove the obstruction and run again to continue")
+    echo -e $(color $red "Remove the obstruction and run again to continue.")
     exit
   else
     echo -e $(color $grn "Creating symlink: $target ")
-    # ln -s $source $target
+
+    if ! $dryrun; then
+      ln -s $source $target
+    fi
+
   fi
 }
 
@@ -38,33 +45,78 @@ function remove_symlink {
   target=$1
 
   if [ -h $target ]; then
-    echo -e $(color $grn "Removing symlink $target")
-    # rm $target
+    echo -e $(color $grn "Removing symlink: $target")
+
+    if ! $dryrun; then
+      rm $target
+    fi
+
   elif [ -e $target ]; then
-    echo -e $(color $ylw "Warning - Found regular file in target location: $target")
+    echo -e $(color $red "Warning - Found regular file in target location: $target")
   else
-    echo -e $(color $grn "Nothing to do for $target")
+    echo -e $(color $ylw "Nothing to do for: $target")
   fi
 }
 
 function install {
-  echo "Installing dotpath symlinks..."
+  echo "Installing dotfile symlinks..."
   while read path; do
-    create_symlink $source_dir/$path $target_dir/$path
+    create_symlink "$source_dir/$path" "$target_dir/$path"
   done < $manifest
+
+  echo "Initializing git submodules..."
+  if ! $dryrun; then
+    git submodule update --init --recursive
+  fi
+
   echo "Done"
 }
 
 function uninstall {
-  echo "Uninstalling dotpath symlinks..."
+  echo "Uninstalling dotfile symlinks..."
   while read path; do
-    remove_symlink $target_dir/$path
+    remove_symlink "$target_dir/$path"
   done < $manifest
   echo "Done"
 }
 
-case $1 in
-  'uninstall' ) uninstall;;
+function update {
+  # TODO:
+  # - Git fetch and merge or rebase changes.
+  # - Add or remove symlinks to match manifest.
+  # - Git update submodules.
+  true
+}
+
+function usage {
+  echo "Usage: $script [<command>] [-d|--dry-run]"
+  echo "Where <command> is 'install', 'uninstall' or 'udate'."
+  exit 0
+}
+
+args=("$@")
+
+for (( i = $# - 1; i >= 0; i-- )); do
+  case ${args[i]} in
+    -d | --dry-run)
+      echo "(DRY RUN: No changes will be made)"
+      dryrun=true
+      unset args[i];;
+    -*)
+      echo "Unrecognised option: ${args[i]}"
+      usage
+      unset args[i];;
+  esac
+done
+
+if [ ${#args[@]} -gt 1 ]; then
+  usage
+fi
+
+case ${args[0]} in
+  ''          ) install;;
   'install'   ) install;;
-  *           ) install;;
+  'uninstall' ) uninstall;;
+  'update'    ) update;;
+  *           ) usage;;
 esac
