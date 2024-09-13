84a475cd28fee0980b46d1d210443539d136eecc
Matt Tarbit <mtarbit@gmail.com>
2024-09-13 11:59:12 +0100

    Hammerspoon: Add helper function for pretty printing tables.

--- source/.hammerspoon/util.lua
+++ source/.hammerspoon/util.lua
@@ -30,3 +30,25 @@ end
 function runAppleScript(s, t)
     return hs.osascript.applescript(template(s, t))
 end
+
+function pprint(obj, depth)
+    depth = depth or 1
+
+    if type(obj) ~= 'table' then
+        return tostring(obj)
+    else
+        local result = ''
+        local indent_inner = string.rep('    ', depth)
+        local indent_outer = string.rep('    ', depth - 1)
+
+        for key, val in pairs(obj) do
+            val = pprint(val, depth + 1)
+            key = type(key) == 'number' and key or '"' .. key .. '"'
+            result = result .. indent_inner .. key .. ' = ' .. val .. ',\n'
+        end
+
+        result = '{\n' .. result .. indent_outer .. '}'
+
+        return result
+    end
+end
