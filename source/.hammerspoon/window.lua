9653e5770b3b02fa155658c5da329fe52e8c5c00
Matt Tarbit <mtarbit@gmail.com>
2026-03-09 10:10:55 +0000

    Hammerspoon: Try to work around issue with whatsapp window size.

--- source/.hammerspoon/window.lua
+++ source/.hammerspoon/window.lua
@@ -1,7 +1,32 @@
 hs.window.animationDuration = 0
 
 -- If this stops working, try restarting the app whose windows aren't resizing correctly.
-function resizeWindowTo(unitrect) hs.window.focusedWindow():moveToUnit(unitrect) end
+function resizeWindowTo(unitrect)
+    local window = hs.window.focusedWindow()
+
+    window:moveToUnit(unitrect)
+
+    if unitrect == hs.layout.right50 then
+        -- Hack around whatsapp's windows now having a minimum width which
+        -- is greater than half of a macbook pro's available screen width,
+        -- so aligning a window to the right of the screen will mean some
+        -- of it is off the edge.
+
+        local expectedFrame = unitrect:fromUnitRect(window:screen():frame())
+        local receivedFrame = window:frame()
+
+        -- local logger = hs.logger.new('window', 'debug')
+        -- logger.i(unitrect)
+        -- logger.i(expectedFrame)
+        -- logger.i(receivedFrame)
+
+        if receivedFrame.w > expectedFrame.w then
+            local x = expectedFrame.x - (receivedFrame.w - expectedFrame.w)
+            local y = expectedFrame.y
+            window:setTopLeft(hs.geometry.point(x, y))
+        end
+    end
+end
 
 function resizeWindowL50() resizeWindowTo(hs.layout.left50) end
 function resizeWindowR50() resizeWindowTo(hs.layout.right50) end
